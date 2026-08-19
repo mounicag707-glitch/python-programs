f64596af0e9d5db2e4c030fbfa08bfdab63f6e14
mounicag707-glitch <mounica.g707@gmail.com>
2026-08-19 21:47:57 +0530

    Add program to find the largest of three numbers

---
+++ large_num.py
@@ -0,0 +1,9 @@
+a=input("enter value af a:")
+b=input("enter value of b:")
+c=input("enter value of c:")
+if a>=b and a>=c:
+    print("a is largest number",largest=a)
+elif b>=c:
+    print("b is a largest number",largest=b)
+else:
+    print("c is a largest number",largest=c)
