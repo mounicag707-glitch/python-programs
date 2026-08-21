60cf297e95a14ee49ae114174f53911a46e900b2
mounicag707-glitch <mounica.g707@gmail.com>
2026-08-21 22:43:09 +0530

    Implement largest number finder using nested if
    Added a script to find the largest of three numbers using nested if statements.

---
+++ largest 3 num using nested if.py
@@ -0,0 +1,14 @@
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+c=int(input("enter value of c:"))
+if a>b:
+  if a>c:
+    print("a is largest number:")
+  else:
+    print("c is largest number:")
+else:
+  if b>c:
+    print("b is largest number:")
+  else:
+    print("c is largest number:")
+        
