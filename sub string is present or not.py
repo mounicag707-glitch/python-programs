48532fe6ed94e1d31bdce63812f5fb18a0242139
mounicag707-glitch <mounica.g707@gmail.com>
2026-08-23 14:40:01 +0530

    Add substring presence check in a string

---
+++ sub string is present or not.py
@@ -0,0 +1,6 @@
+string=input("enter a string:")
+substring=input("enter a substring:")
+if substring in string:
+  print("substring is present in string")
+else:
+  print("substring is not present")
