12c0feefa4392ad192824ff220b09910a0b2d797
mounicag707-glitch <mounica.g707@gmail.com>
2026-08-23 14:43:51 +0530

    Add built-in functions demonstration for a list

---
+++ built in functions.py
@@ -0,0 +1,7 @@
+numbers=[10,5,25,20,15]
+print("list=",numbers)
+print("length=",len(numbers))
+print("maximum=",max(numbers))
+print("minimum=",min(numbers))
+print("sum=",sum(numbers))
+print("sorted list=",sorted(numbers))
