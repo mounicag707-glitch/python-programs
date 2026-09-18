22266c2d0a51d449bc5488ebf5f048b6ad787d51
mounicag707-glitch <mounica.g707@gmail.com>
2026-09-18 19:33:22 +0530

    Add script to check starting and ending characters

---
+++ check starting and ending characters.py
@@ -0,0 +1,9 @@
+text=input("enter a string:")
+if text.startswith("A"):
+  print("string starts with A")
+else:
+  print("string does not starts with A")
+if text.endswith("g"):
+  print("string ends with g")
+else:
+  print("string does not ends with g")
